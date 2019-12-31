c2819101975a11f9fd645e4e4f12a487f043e8d9
matsushin <matsushin.dev@gmail.com>
2019-12-31 09:35:01 +0900

    設定リファクタ

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 set -ex
 
-SECURITY_GROUP_NAME="rails-ci-sample-sec"
+SECURITY_GROUP_ID="sg-007702475d97eac54"
 IP=`curl -f -s ifconfig.me`
 
-trap "aws ec2 revoke-security-group-ingress --group-name ${SECURITY_GROUP_NAME} --protocol tcp --port 22 --cidr ${IP}/32" 0 1 2 3 15
-aws ec2 authorize-security-group-ingress --group-name ${SECURITY_GROUP_NAME} --protocol tcp --port 22 --cidr ${IP}/32
-bundle exec cap prod deploy
+trap "aws ec2 revoke-security-group-ingress --region ap-northeast-1 --group-id ${SECURITY_GROUP_ID} --protocol tcp --port 22 --cidr ${IP}/32" 0 1 2 3 15
+aws ec2 authorize-security-group-ingress --region ap-northeast-1 --group-id ${SECURITY_GROUP_ID} --protocol tcp --port 22 --cidr ${IP}/32
+bundle exec cap production deploy
