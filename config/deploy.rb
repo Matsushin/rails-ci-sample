bd7266480d471424803d191a3c8ffa83d48bc20c
Matsushin <matsu1983@gmail.com>
2020-01-18 14:57:12 +0900

    設定変更

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,7 +14,7 @@ set :branch, 'add-capistrano2'
 set :deploy_to, '/var/www/rails-ci-sample'
 
 # シンボリックリンクをはるファイル。(※後述)
-set :linked_files, fetch(:linked_files, []).push('config/master.key')
+set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/master.key')
 
 # シンボリックリンクをはるフォルダ。(※後述)
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
